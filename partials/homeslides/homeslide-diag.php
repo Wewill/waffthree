e2203b6be879a657edff9567e936e88fa8070c5e
Wilhem Arnoldy <contact@wilhemarnoldy.fr>
2023-05-04 16:20:22 +0200

    Update wording

--- partials/homeslides/homeslide-diag.php
+++ partials/homeslides/homeslide-diag.php
@@ -192,7 +192,7 @@ $slide_colors 		= array();
 						<a class="bg-action-3 color-action-1 py-3 px-5 py-sm-4 px-sm-9 rounded-start rounded-pill text-white h4 text-end" href="#">Prendre rendez-vous</a>
 						<a class="bg-color-gray py-3 px-5 py-sm-4 px-sm-9 rounded-start rounded-pill text-white h4 text-end" href="#">Régler en ligne</a>
 						<a class="bg-action-2 py-3 px-5 py-sm-4 px-sm-9 rounded-start rounded-pill text-white h4 text-end" href="#">Consulter mes résultats</a>				
-						<a class="bg-action-1 py-3 px-5 py-sm-4 px-sm-9 rounded-start rounded-pill text-white h4 text-end" href="#">Faire son pré-accueil</a>				
+						<a class="bg-action-1 py-3 px-5 py-sm-4 px-sm-9 rounded-start rounded-pill text-white h4 text-end" href="#">Faire mon pré-accueil</a>				
 					</div>
 				</div>
 
