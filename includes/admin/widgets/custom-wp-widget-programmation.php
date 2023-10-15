402ce50c61f93f0434b83f4186e72787e7a26170
Wilhem Arnoldy <contact@wilhemarnoldy.fr>
2023-10-15 15:53:08 +0200

    New programmation widget from dev

--- includes/admin/widgets/custom-wp-widget-programmation.php
+++ includes/admin/widgets/custom-wp-widget-programmation.php
@@ -39,41 +39,12 @@ class WP_Widget_Programmation extends WP_Widget {
 		// This is where you run the code and display the output
 		global $current_edition, $previous_editions, $current_edition_id, $current_edition_films_are_online;
 
-		if ( !$current_edition_films_are_online ) : ?>	
+		// Get user loggued in
+		$user = wp_get_current_user();
+		$allowed_roles = array('fifam_editor', 'fifam_admin', 'fifam_subscriber', 'administrator');
 
-			<div class="modal fade widget_programmation current_edition_films_are_offline <?= $instance['classes'] ?>" id="programmationModal" tabindex="-1" role="dialog" aria-labelledby="programmationModalLabel" aria-hidden="true">
-			<div class="modal-dialog m-0 max-w-100 ml-auto" role="document">
-					<div class="modal-content bg-transparent border-0 rounded-0 color-light text-white" style="overflow: hidden;height: 100vh; position: relative;">
-					<div class="modal-header sticky-top container-fluid">
-							<div class="row g-0 align-items-center">
-								<div class="col-md-5 col-lg-7 d-none d-md-block">
-									<a class="close-icon color-light lead ml-5" data-bs-dismiss="modal" aria-label="Close">
-										<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-										<path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"></path>
-										</svg>
-									</a>
-								</div>
-								<div class="col-md-7 col-lg-5 p-0 col-days order-1">
-									<div class="bg-action-1 text-center text-white link-light d-none d-xl-block">
-										<div class="p-2"><a class="prog-title headline" data-bs-dismiss="modal" aria-label="Close" id="programmationModalLabel"><?= esc_html__( 'Programmation', 'waff' ) ?></a></div>
-									</div>	
-								</div>
-							</div>
-						</div>
-						<div class="modal-body d-flex align-items-center justify-content-center" style="position: relative;overflow-x: hidden;overflow-y: scroll;height: 100%;" id="">
-							<p><?= $text ?></p>
-						</div>
-						<div class="modal-footer">
-						<button type="button" class="btn btn-action-1"><?= esc_html__( 'View archives', 'waff' ) ?></button>
-						<button type="button" class="btn btn-action-1"><?= esc_html__( 'View blog', 'waff' ) ?></button>
-						<button type="button" class="btn btn-outline-action-1" data-bs-dismiss="modal"><?= esc_html__( 'Close', 'waff' ) ?></button>
-						</div>
-					</div>
-				</div>
-			</div>
-
-		<?php else : 
-
+		if ( $current_edition_films_are_online || !empty(array_intersect($allowed_roles, $user->roles )) ) :	
+		
 			// Rooms
 			$args = array(
 				'taxonomy' => 'room',
@@ -242,6 +213,7 @@ class WP_Widget_Programmation extends WP_Widget {
 					$p_is_guest 			= types_render_field( 'p-is-guest', array() );
 					$p_e_guest_contact 		= types_render_field( 'p-e-guest-contact', array() );
 					$p_e_guest_contact_raw  = get_post_meta( $id, 'wpcf-p-e-guest-contact', false );
+					$p_is_debate 			= types_render_field( 'p-is-debate', array() ); //#43
 					$p_young_public 		= types_render_field( 'p-young-public', array() );
 					$p_highlights 			= types_render_field( 'p-highlights', array() );
 					$p_translator 			= types_render_field( 'p-translator', array() );
@@ -267,10 +239,37 @@ class WP_Widget_Programmation extends WP_Widget {
 					}
 
 					// Get film meta values
-					$f_id					= (( true === $has_film )?$connections[0]:0);
-					$f_title 				= (( get_the_title($f_id) )?get_the_title($f_id):'');
-					$f_french_operating_title = get_post_meta( $f_id, 'wpcf-f-french-operating-title', true );
-					$f_movie_length 		= get_post_meta( $f_id, 'wpcf-f-movie-length', true );
+					$f_id						= (( true === $has_film )?$connections[0]:0);
+					$f_title 					= (( get_the_title($f_id) )?get_the_title($f_id):'');
+					$f_french_operating_title 	= get_post_meta( $f_id, 'wpcf-f-french-operating-title', true );
+					$f_movie_length 			= get_post_meta( $f_id, 'wpcf-f-movie-length', true );
+					$f_country 					= get_post_meta( $f_id, 'wpcf-f-country', true ); //#43
+					$f_co_production_country 	= get_post_meta( $f_id, 'wpcf-f-co-production-country', false ); //#43
+					$f_author 					= get_post_meta( $f_id, 'wpcf-f-author', true ); //#43
+					$f_production_year 			= get_post_meta( $f_id, 'wpcf-f-production-year', true ); //#43
+
+					echo $f_premiere 				= get_post_meta( $f_id, 'wpcf-f-premiere', true ); //#43
+					echo $f_catalog_tag 			= get_post_meta( $f_id, 'wpcf-f-catalog-tag', true ); //#43
+					$f_available_formats 		= get_post_meta( $f_id, 'wpcf-f-available-formats', true ); //#43
+
+					print_r($f_available_formats,true);
+
+					$f_poster 					= get_post_meta( $f_id, 'wpcf-f-film-poster', true ); //#43
+
+					$f_co_production_country 	= types_render_field( 'f-co-production-country', array('item' => $f_id) ); //#43
+
+
+					$f_poster 						= get_post_meta( $f_id, 'wpcf-f-film-poster', true ); //#43
+					$f_poster_id 					= WaffTwo\Core\waff_get_image_id_by_url($f_poster);
+					$f_poster_url 					= wp_get_attachment_image_url( $f_poster_id, "film-poster" ); // OK
+					$f_poster_img 					= wp_get_attachment_image( $f_poster_id, "film-poster", "", array( "class" => "img-responsive" ) ); // OK
+
+					$f_featured_img 				= get_the_post_thumbnail( $f_id, 'film-poster');
+					$f_poster_img					= ( $f_poster != '') ? $f_poster_img : $f_featured_img;
+
+					// Not working 
+					//echo $f_poster 				= types_render_field( 'f-film-poster', array( 'output' => 'raw', 'item' => $f_id, 'size' => 'thumbnail', 'alt' => esc_html($f_title), 'style' => 'height: 150px; object-fit: cover;', 'class' => 'img-fluid' ) ); //#43
+					//echo $f_poster 				= do_shortcode( "[wpv-post-featured-image item='".$f_poster."' size='custom' width='50' height='50' crop='true' output='url']" );
 
 					// Get terms
 					$f_sections 				= get_the_terms( $f_id, 'section' );
@@ -288,6 +287,7 @@ class WP_Widget_Programmation extends WP_Widget {
 						'p_is_guest' 						=> $p_is_guest,
 						'p_e_guest_contact' 				=> $p_e_guest_contact, //9
 						'p_e_guest_contact_raw' 			=> $p_e_guest_contact_raw,
+						'p_is_debate' 						=> $p_is_debate, //#43
 						'p_young_public' 					=> $p_young_public,
 						'p_highlights' 						=> $p_highlights,
 						'p_translator' 						=> $p_translator,
@@ -300,6 +300,15 @@ class WP_Widget_Programmation extends WP_Widget {
 						'f_french_operating_title' 			=> $f_french_operating_title,
 						'f_movie_length' 					=> $f_movie_length,
 						'f_sections' 						=> $f_sections,
+						'f_country' 						=> $f_country, //#43
+						'f_co_production_country' 			=> $f_co_production_country, //#43
+						'f_author' 							=> $f_author, //#43
+						'f_production_year' 				=> $f_production_year, //#43
+						'f_poster' 							=> $f_poster, //#43
+						'f_poster_img' 						=> $f_poster_img, //#43
+						'f_premiere' 						=> $f_premiere, //#43
+						'f_catalog_tag' 					=> $f_catalog_tag, //#43
+						'f_available_formats' 				=> $f_available_formats, //#43
 					);
 
 					// Final Loop
@@ -388,8 +397,10 @@ class WP_Widget_Programmation extends WP_Widget {
 												
 												// Sections
 												$html_f_section = '';
+												$last_f_section_color = '';
 												if (array_key_exists('f_sections', $the_day_room_projections) && is_array($the_day_room_projections['f_sections'])) foreach($the_day_room_projections['f_sections'] as $f_section) {
 													$f_section_color = get_term_meta( $f_section->term_id, 'wpcf-s-color', true );
+													if ( $f_section_color != '' ) $last_f_section_color = $f_section_color;
 													$f_section_edition = get_term_meta( $f_section->term_id, 'wpcf-select-edition', true );
 													if ( $current_edition_id == $f_section_edition ) // Only current edition sections
 													$html_f_section .= sprintf('<a href="%s" %s class="dot-section" data-bs-toggle="tooltip" data-bs-container=".modal" data-bs-title="%s" data-bs-original-title="" title="">•</a>',
@@ -405,19 +416,30 @@ class WP_Widget_Programmation extends WP_Widget {
 
 												if ( $the_day_room_projections['p_young_public'] != '' ) 	$html_f_tags .= ' <i class="icon icon-young" data-bs-toggle="tooltip" data-bs-container=".modal-body" title="Parents-enfants"></i>'; 
 												if ( $the_day_room_projections['p_is_guest'] != '' ) 		$html_f_tags .= ' <i class="icon icon-guest" data-bs-toggle="tooltip" data-bs-html="true" data-bs-container=".modal-body" title="En présence de • <strong>'.$the_day_room_projections['p_e_guest_contact'].'</strong>"></i>'; 
-												if ( $the_day_room_projections['p_highlights'] != '' )		$html_f_tags .= ' <i class="icon icon-sun" data-bs-toggle="tooltip" data-container=".modal-body" title="Temps-fort"></i>'; 
+												if ( $the_day_room_projections['p_is_debate'] != '' ) 		$html_f_tags .= ' <i class="icon icon-mic" data-bs-toggle="tooltip" data-bs-html="true" data-bs-container=".modal-body" title="Séance avec débat"></i>'; 
+												if ( $the_day_room_projections['p_highlights'] != '' )		$html_f_tags .= ' <i class="icon icon-sun" data-bs-toggle="tooltip" data-bs-container=".modal-body" title="Temps-fort"></i>'; 
 												if ( $the_day_room_projections['p_tag'] != '' ) 			$html_f_tags .= ' <i class="icon icon-warning text-danger" data-bs-toggle="tooltip" data-bs-html="true" data-bs-container=".modal-body" title="'.$the_day_room_projections['p_tag'].'"></i>'; 
+												if ( $the_day_room_projections['f_premiere'] != '' ) 		$html_f_tags .= ' <i class="icon icon-premiere text-danger" data-bs-toggle="tooltip" data-bs-html="true" data-bs-container=".modal-body" title="Première '.$the_day_room_projections['f_premiere'].'"></i>'; 
+												if ( $the_day_room_projections['f_catalog_tag'] == 7 )		$html_f_tags .= ' <i class="icon icon-avantpremiere text-danger" data-bs-toggle="tooltip" data-bs-html="true" data-bs-container=".modal-body" title="'.$the_day_room_projections['f_catalog_tag'].'"></i>'; 
+												
+												// Formats 
+												if ( $the_day_room_projections['f_available_formats'] != '' ) {
+													$format = $the_day_room_projections['f_available_formats'][0];
+													if ($format['vost'] == 'en' && $format['vost'] != '') {
+														$html_f_tags .= ' <i class="icon icon-vo text-danger" data-bs-toggle="tooltip" data-bs-html="true" data-bs-container=".modal-body" title="VOSTA"></i>'; 
+													}
+												}
 
 												// 'p_is_guest' 						=> $p_is_guest,
-												// 'p_e_guest_contact' 				=> $p_e_guest_contact, //9
-												// 'p_e_guest_contact_raw' 			=> $p_e_guest_contact_raw,
+												// 'p_e_guest_contact' 					=> $p_e_guest_contact, //9
+												// 'p_e_guest_contact_raw' 				=> $p_e_guest_contact_raw,
 												// 'p_young_public' 					=> $p_young_public,
 												// 'p_highlights' 						=> $p_highlights,
 												// 'p_translator' 						=> $p_translator,
-												// 'p_tag' 							=> $p_tag,
+												// 'p_tag' 								=> $p_tag,
 
 												// Print film
-												printf('<dt class="col-2" data-p-id="%d"><p class="length" data-p-begin="%s">%s</p></dt>
+												/*printf('<dt class="col-2" data-p-id="%d"><p class="length" data-p-begin="%s">%s</p></dt>
 												<dd class="col-10" data-p-id="%d">
 													<p>
 														<a href="%s" class="%s">%s</a>&nbsp;
@@ -436,7 +458,44 @@ class WP_Widget_Programmation extends WP_Widget {
 												(( $the_day_room_projections['f_movie_length'] != '' )?'<span class="length">'.$the_day_room_projections['f_movie_length'].'\'</span>':''),
 												$html_f_section,
 												$html_f_tags
+												);*/
+
+												// Print film
+												printf('<dt class="col-2 mb-2" data-p-id="%d"><a href="%s">%s</a></dt>
+												<dd class="col-10 mb-3" data-p-id="%d">
+													<p class="length"><span class="">%s</span> <span class="normal op-5"> › %s</span></p>
+													<p>
+														<span class="last_f_section_color" %s>
+															<a href="%s" class="title %s">%s</a>&nbsp;
+															%s
+														</span>
+														%s
+														%s
+														%s
+														%s
+														%s
+														%s
+													</p>
+												</dd><hr class="bg-layoutcolor --op-1"/>',
+												esc_attr( $the_day_room_projections['p_id'] ),
+												get_permalink( $the_day_room_projections['f_id'] ),
+												$the_day_room_projections['f_poster_img'],
+												esc_attr( $the_day_room_projections['p_id'] ),
+												esc_html( $the_day_room_projections['p_start_and_stop_time_raw']['begin'] ),
+												esc_html( $the_day_room_projections['p_start_and_stop_time_raw']['end'] ),
+												(( $last_f_section_color != '' )?'style="color: '.$last_f_section_color.';"':''),
+												(($the_day_room_projections['p_connections'] == 0 || $the_day_room_projections['p_has_film'] == '' )?get_permalink( $the_day_room_projections['p_id'] ):get_permalink( $the_day_room_projections['f_id'] )),
+												(($the_day_room_projections['p_connections'] == 0 || $the_day_room_projections['p_has_film'] == '' )?'text-link btn-link disabled':'text-link'),
+												esc_html(( $the_day_room_projections['f_french_operating_title'] != '' )?$the_day_room_projections['f_french_operating_title'].' ('.$the_day_room_projections['f_title'].')':$the_day_room_projections['f_title']).(($the_day_room_projections['p_connections'] == 0 || $the_day_room_projections['p_has_film'] == '' )?' <i class="icon icon-down-right-light ml-2 ms-2"></i>':''),
+												(( $the_day_room_projections['f_author'] != '' )?'<span class="article">DE</span>&nbsp;<span class="director">'.$the_day_room_projections['f_author']['lastname'].' '.$the_day_room_projections['f_author']['firstname'].'</span>':''),
+												(( $the_day_room_projections['f_country'] != '' )?'・ <span class="country">'.$the_day_room_projections['f_country'].'</span>':''),
+												(( $the_day_room_projections['f_co_production_country'] != '' )?'・ <span class="co_production_country">'.$the_day_room_projections['f_co_production_country'].'</span>':''),
+												(( $the_day_room_projections['f_production_year'] != '' )?'・ <span class="year muted">'.$the_day_room_projections['f_production_year'].'</span>':''),
+												(( $the_day_room_projections['f_movie_length'] != '' )?'・ <span class="length">'.$the_day_room_projections['f_movie_length'].'\'</span>':''),
+												$html_f_section,
+												$html_f_tags
 												);
+												
 
 											//}
 										}
@@ -481,8 +540,41 @@ class WP_Widget_Programmation extends WP_Widget {
 			</div>	
 		</div>
 		<!-- END: .programmation-button  -->
+
+		<?php else : ?>
+
+		<div class="modal fade widget_programmation current_edition_films_are_offline <?= $instance['classes'] ?>" id="programmationModal" tabindex="-1" role="dialog" aria-labelledby="programmationModalLabel" aria-hidden="true">
+			<div class="modal-dialog m-0 max-w-100 ml-auto" role="document">
+					<div class="modal-content bg-transparent border-0 rounded-0 color-light text-white" style="overflow: hidden;height: 100vh; position: relative;">
+					<div class="modal-header sticky-top container-fluid">
+							<div class="row g-0 align-items-center">
+								<div class="col-md-5 col-lg-7 d-none d-md-block">
+									<a class="close-icon color-light lead ml-5" data-bs-dismiss="modal" aria-label="Close">
+										<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+										<path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"></path>
+										</svg>
+									</a>
+								</div>
+								<div class="col-md-7 col-lg-5 p-0 col-days order-1">
+									<div class="bg-action-1 text-center text-white link-light d-none d-xl-block">
+										<div class="p-2"><a class="prog-title headline" data-bs-dismiss="modal" aria-label="Close" id="programmationModalLabel"><?= esc_html__( 'Programmation', 'waff' ) ?></a></div>
+									</div>	
+								</div>
+							</div>
+						</div>
+						<div class="modal-body d-flex align-items-center justify-content-center" style="position: relative;overflow-x: hidden;overflow-y: scroll;height: 100%;" id="">
+							<p>#<?= print_r(var_dump(empty(array_intersect($allowed_roles, $user->roles ))),true) . $text ?></p>
+						</div>
+						<div class="modal-footer">
+						<button type="button" class="btn btn-action-1"><?= esc_html__( 'View archives', 'waff' ) ?></button>
+						<button type="button" class="btn btn-action-1"><?= esc_html__( 'View blog', 'waff' ) ?></button>
+						<button type="button" class="btn btn-outline-action-1" data-bs-dismiss="modal"><?= esc_html__( 'Close', 'waff' ) ?></button>
+						</div>
+					</div>
+				</div>
+			</div>
 			
-		<?php endif; // !$current_edition_films_are_online ?> 
+		<?php endif; // $current_edition_films_are_online ?> 
 
 		<!-- END: Programmation Widget  -->
 			
