80339ba18dce0b6d8711fc1ae4c0256e1be932bd
Wilhem Arnoldy <contact@wilhemarnoldy.fr>
2025-05-28 17:31:08 +0200

    refactor: Update admin JavaScript to use native DOM methods and remove jQuery dependency

--- dist/js/admin/custom-admin.js
+++ dist/js/admin/custom-admin.js
@@ -6,13 +6,3 @@
 // 	"use strict";
 // 	console.log('Ready');
 // });
-
-document.addEventListener("DOMContentLoaded", function () {
-  const notices = document.querySelectorAll(".notice");
-
-  notices.forEach(function (notice) {
-    if (notice.textContent.includes("Meta Box")) {
-      notice.style.display = "none";
-    }
-  });
-});
