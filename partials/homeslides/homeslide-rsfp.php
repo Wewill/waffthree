b6334965359738e128e658f1dffeaf7a285bf6f2
Wilhem Arnoldy <contact@wilhemarnoldy.fr>
2025-06-12 11:07:23 +0200

    Still better homeslide responsive #RSFP

--- partials/homeslides/homeslide-rsfp.php
+++ partials/homeslides/homeslide-rsfp.php
@@ -30,7 +30,7 @@ $homeslide_image = ( !empty($homeslide_images) ) ? reset($homeslide_images) : fa
 <section id="slick-homeslide" class="mb-0 mb-sm-1 mb-lg-2 contrast--light">
 	<div class="container-fluid px-0">
 		<div class="row g-0 align-items-center vh-50" style="@media (min-width:768px){ min-height: 460px; }"> <!-- .vh-50 hack--> 
-			<div class="col-lg --order-last order-2 order-lg-last overflow-hidden position-relative pt-10 pt-md-0" data-aos="fade-right">
+			<div class="col-lg --order-last order-2 order-lg-last overflow-hidden position-relative" data-aos="fade-right">
 				
 				<!-- slick-homeslide images-->
 				<div class="slider-nav">
