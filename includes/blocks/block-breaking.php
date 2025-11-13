dc2db15f3aac3062888727830571d4fa446540e2
Wilhem Arnoldy <contact@wilhemarnoldy.fr>
2025-11-13 22:20:25 +0100

    improve: design responsive

--- includes/blocks/block-breaking.php
+++ includes/blocks/block-breaking.php
@@ -120,11 +120,11 @@ function wa_breaking_callback( $attributes ) {
 						<div class="w-100">
 							<div class="w-100 w-lg-50">
 								<p class="card-date fw-bold <?= mb_get_block_field( 'waff_b_subtitle_1_class' )?mb_get_block_field( 'waff_b_subtitle_1_class' ):'text-transparent-color-layout'; ?> mt-1 mb-0"><?= mb_get_block_field( 'waff_b_subtitle_1' ) ?></p>
-								<h2 class="card-title"><a href="#" class="stretched-link link-white text-hover-color-light"><?= mb_get_block_field( 'waff_b_title_1' ) ?></a></h2>
+								<h2 class="card-title"><a href="<?php echo esc_url( mb_get_block_field( 'waff_b_moreurl_1' )); ?>" class=" link-white text-hover-color-light"><?= mb_get_block_field( 'waff_b_title_1' ) ?></a></h2>
 							</div>
-							<div class="card-text fw-bold"><?= waff_do_markdown(mb_get_block_field( 'waff_b_content_1' )) ?></div>
+							<div class="card-text --fw-bold mt-4"><?= waff_do_markdown(mb_get_block_field( 'waff_b_content_1' )) ?></div>
 							<?php if ( mb_get_block_field( 'waff_b_morelink_1' ) == 1 ) : ?>
-							<a class="btn btn-action-3 btn-lg mt-4 btn-transition-scale" href="<?php echo esc_url( mb_get_block_field( 'waff_b_moreurl_1' )); ?>"><?php esc_html_e( 'More...', 'waff' ); ?></a>
+							<a class="btn btn-action-3 btn-lg mt-4 stretched-link btn-transition-scale" href="<?php echo esc_url( mb_get_block_field( 'waff_b_moreurl_1' )); ?>"><?php esc_html_e( 'More...', 'waff' ); ?></a>
 							<?php endif; ?>
 						</div>
 					</div>
@@ -157,11 +157,11 @@ function wa_breaking_callback( $attributes ) {
 						<div class="w-100">
 							<div class="w-100 w-lg-50">
 								<p class="card-date fw-bold <?= mb_get_block_field( 'waff_b_subtitle_2_class' )?mb_get_block_field( 'waff_b_subtitle_2_class' ):'text-transparent-color-layout'; ?> mt-1 mb-0"><?= mb_get_block_field( 'waff_b_subtitle_2' ) ?></p>
-								<h3 class="card-title"><a href="#" class="stretched-link link-white text-hover-color-light"><?= mb_get_block_field( 'waff_b_title_2' ) ?></a></h3>
+								<h3 class="card-title"><a href="<?php echo esc_url( mb_get_block_field( 'waff_b_moreurl_2' )); ?>" class="link-white text-hover-color-light"><?= mb_get_block_field( 'waff_b_title_2' ) ?></a></h3>
 							</div>
-							<div class="card-text fw-bold"><?= waff_do_markdown(mb_get_block_field( 'waff_b_content_2' )) ?></div>
+							<div class="card-text --fw-bold mt-4"><?= waff_do_markdown(mb_get_block_field( 'waff_b_content_2' )) ?></div>
 							<?php if ( mb_get_block_field( 'waff_b_morelink_2' ) == 1 ) : ?>
-							<a class="btn btn-action-3 btn-lg mt-4 btn-transition-scale" href="<?php echo esc_url( mb_get_block_field( 'waff_b_moreurl_2' )); ?>"><?php esc_html_e( 'More...', 'waff' ); ?></a>
+							<a class="btn btn-action-3 btn-lg mt-4 stretched-link btn-transition-scale" href="<?php echo esc_url( mb_get_block_field( 'waff_b_moreurl_2' )); ?>"><?php esc_html_e( 'More...', 'waff' ); ?></a>
 							<?php endif; ?>
 						</div>
 					</div>
